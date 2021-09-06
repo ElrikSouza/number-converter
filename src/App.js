65c57f4688a67eaa6ec81ca5a43295417a4f3ad0
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-09-06 10:51:53 -0400

    updated app routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Conversion } from "./conversion-page";
+import { ToFloatPage } from "./floats/tofloat-page";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/float">
+          <ToFloatPage />
+        </Route>
         <Route path="/">
           <Conversion />
         </Route>
